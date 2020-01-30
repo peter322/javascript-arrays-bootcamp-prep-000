efdb68b65e44946a491f3a2b42b2bd8fe0fbcee8
peter322 <peter322@uwm.edu>
2020-01-30 20:21:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  console.log(array.pop);
+}
